f3edefa8b54ee6a129a50d97e53b301e1ef313ab
Danny <danny@mymy.cards>
2022-05-16 22:49:45 +0800

    Completed: Unit 2: Building app UI: Kotlin fundamentals: 5. Use function types and lambda expressions in Kotlin Practice 4: Complete

--- app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path1_Practice.kt
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path1_Practice.kt
@@ -15,12 +15,26 @@ fun main() {
 
     val isMonday = true
 
-    println("The movie ticket price for a person aged $child is  \$${ticketPrice(child, isMonday)}.")
-    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
-    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+//    println("The movie ticket price for a person aged $child is  \$${ticketPrice(child, isMonday)}.") // Comment
+//    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.") // Comment
+//    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.") // Comment
+
+    // Practice 4
+    val CtoF: (Double) -> Double = { // Celsius to Fahrenheit
+        9*it/5 + 32
+    }
+    val KtoC: (Double) -> Double = { // Kelvin to Celsius
+        it - 273.15
+    }
+    val FtoK: (Double) -> Double = { // Fahrenheit to Kelvin
+        5*(it-32)/9 + 273.15
+    }
+//    printFinalTemperature(27.0, "Celsius", "Fahrenheit", CtoF) // Comment
+//    printFinalTemperature(350.0, "Kelvin", "Celsius", KtoC) // Comment
+//    printFinalTemperature(10.0, "Fahrenheit", "Kelvin", FtoK) // Comment
 }
 
-// Practice 2
+// Practice 2 : Mobile notification
 fun printNotificationSummary(numberOfMessages: Int) {
     if (numberOfMessages < 100 ) {
         println("You have $numberOfMessages notifications.")
@@ -29,7 +43,7 @@ fun printNotificationSummary(numberOfMessages: Int) {
     }
 }
 
-// Practice 3
+// Practice 3 : Movie-ticket price
 fun ticketPrice(age: Int, isMonday: Boolean): Int {
     return when (age) {
         in 0..12 -> 15
@@ -42,4 +56,15 @@ fun ticketPrice(age: Int, isMonday: Boolean): Int {
         in 61..100 -> 20
         else -> -1
     }
+}
+
+// Practice 4 : Temperature converter
+fun printFinalTemperature(
+    initialMeasurement: Double,
+    initialUnit: String,
+    finalUnit: String,
+    conversionFormula: (Double) -> Double
+) {
+    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
+    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
 }
