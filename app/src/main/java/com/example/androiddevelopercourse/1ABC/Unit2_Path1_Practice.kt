7ab99f6ca5097230d972c3971dbff1adfa7d89b9
Danny <danny@mymy.cards>
2022-05-17 00:35:05 +0800

    Completed: Unit 2: Building app UI: Kotlin fundamentals: 5. Use function types and lambda expressions in Kotlin 9: Complete; Corrected code based on solutions

--- app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path1_Practice.kt
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path1_Practice.kt
@@ -33,6 +33,16 @@ fun main() {
 //    printFinalTemperature(350.0, "Kelvin", "Celsius", KtoC) // Comment
 //    printFinalTemperature(10.0, "Fahrenheit", "Kelvin", FtoK) // Comment
 
+    // Above correct, but here takes advantage of trailing lambda syntax
+//    printFinalTemperature(27.0, "Celsius", "Fahrenheit") { 9.0 / 5.0 * it + 32 }
+//    printFinalTemperature(350.0, "Kelvin", "Celsius") { it - 273.15 }
+//    printFinalTemperature(10.0, "Fahrenheit", "Kelvin") { 5.0 / 9.0 * (it - 32) + 273.15 }
+
+    // Practice 5
+    val brunoSong = Song("We Don't Talk About Bruno", "Encanto Cast", 2022, 1_000_000)
+//    brunoSong.SongDetails() // Comment
+//    println(brunoSong.isPopular) // Comment
+
     // Practice 6
     val amanda = Person("Amanda", 33, "play tennis", null)
     val atiqah = Person("Atiqah", 28, "climb", amanda)
@@ -40,17 +50,27 @@ fun main() {
 //    amanda.showProfile() // Comment
 //    atiqah.showProfile() // Comment
 
+    // Practice 7
+    val newFoldablePhone = FoldablePhone(false)
+
+//    newFoldablePhone.switchOn() // Comment
+//    newFoldablePhone.checkPhoneScreenLight() // Comment
+//    newFoldablePhone.unfoldPhone() // Comment
+//    newFoldablePhone.switchOn() // Comment
+//    newFoldablePhone.checkPhoneScreenLight() // Comment
+
     // Practice 8
     val winningBid = Bid(5000, "Private Collector")
 
-//    println("Item A is sold at ${auctionPrice(winningBid, 2000)}.") // Comment
-//    println("Item B is sold at ${auctionPrice(null, 3000)}.") // Comment
+    println("Item A is sold at ${auctionPrice(winningBid, 2000)}.") // Comment
+    println("Item B is sold at ${auctionPrice(null, 3000)}.") // Comment
 }
 
 // Practice 2 : Mobile notification
 fun printNotificationSummary(numberOfMessages: Int) {
     if (numberOfMessages < 100 ) {
-        println("You have $numberOfMessages notifications.")
+//        println("You have $numberOfMessages notifications.") // Original; no curly brackets but still worked
+        println("You have ${numberOfMessages} notifications.")
     } else {
         println("Your phone is blowing up! You have 99+ notifications.")
     }
@@ -60,12 +80,13 @@ fun printNotificationSummary(numberOfMessages: Int) {
 fun ticketPrice(age: Int, isMonday: Boolean): Int {
     return when (age) {
         in 0..12 -> 15
-        in 13..60 ->
-            if (isMonday) {
-                25
-            } else {
-                30
-            }
+//        in 13..60 -> // Original; Correct but below is simplied
+//            if (isMonday) {
+//                25
+//            } else {
+//                30
+//            }
+        in 13..60 -> if (isMonday) 25 else 30
         in 61..100 -> 20
         else -> -1
     }
@@ -84,13 +105,18 @@ fun printFinalTemperature(
 
 // Practice 5 : Song catalog
 class Song(val title: String, val artist: String, val year: Int, val playCount: Int) {
-    fun SongPopularity() {
-        if (playCount < 1000 ) {
-            println("Song is unpopular")
-        } else {
-            println("Song is popular")
-        }
-    }
+    // uses customer getter function and a method that prints the description of itself
+//        fun SongPopularity() {
+//        if (playCount < 1000 ) {
+//            println("Song is unpopular")
+//        } else {
+//            println("Song is popular")
+//        }
+//    }
+    val isPopular: Boolean
+        get() = playCount >= 1000
+
+    // Correct
     fun SongDetails() {
         println("$title, performed by $artist, was released in $year.")
     }
@@ -101,13 +127,21 @@ class Person(val name: String, val age: Int, val hobby: String?, val referrer: P
     fun showProfile() {
         println("Name: $name")
         println("Age: $age")
-        val textHobby = ""
-        if (referrer != null) {
-            println("Likes to $hobby. Has a referrer named ${referrer.name}, who likes to ${referrer.hobby}")
-        } else {
-            println("Likes to $hobby. Doesn't have a referrer.")
+        // Missed out null check for hobby
+        if (hobby != null) {
+            println("Likes to $hobby. ")
         }
-        println()
+        // Null checks for both referrer, and the referrer's property hobby
+        // print continues text on same row unlike println
+        if (referrer != null) {
+            print("Has a referrer named ${referrer.name}, who likes to ${referrer.hobby}")
+            if (referrer.hobby != null) {
+                print(", who likes to ${referrer.hobby}")
+            } else { print(".") }
+        } else {
+            print("Doesn't have a referrer.")
+        }
+        print("\n\n")
     }
 }
 
@@ -129,6 +163,7 @@ open class Phone(var isScreenLightOn: Boolean = false) {
 
 class FoldablePhone(deviceIsScreenLightOn: Boolean, var deviceFolded: Boolean = true) :
         Phone(isScreenLightOn = deviceIsScreenLightOn) {
+
     override fun switchOn() {
         if (!deviceFolded) { // If device is NOT folded
             super.switchOn()
@@ -148,10 +183,14 @@ class FoldablePhone(deviceIsScreenLightOn: Boolean, var deviceFolded: Boolean = 
 class Bid(val amount: Int, val bidder: String)
 
 fun auctionPrice(bid: Bid?, minimumPrice: Int): Int {
-    if (bid != null) {
-        return bid.amount
-    } else {
-        return minimumPrice
-    }
+    // Technically correct
+//    if (bid != null) {
+//        return bid.amount
+//    } else {
+//        return minimumPrice
+//    }
+
+    // Solution uses safe call and elvis operator to correct price
+    return bid?.amount ?: minimumPrice
 }
 
