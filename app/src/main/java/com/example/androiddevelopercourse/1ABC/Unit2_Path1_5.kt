f776978f3a3b81d6e5e59468a6534823311b143d
Danny <danny@mymy.cards>
2022-05-16 21:32:34 +0800

    Completed: Unit 2: Building app UI: Kotlin fundamentals: 5. Use function types and lambda expressions in Kotlin Complete: 3. Use functions as a data type

--- app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path1_5.kt
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path1_5.kt
@@ -12,7 +12,37 @@ fun main() {
     val trickFunctionV3 = trickv3 // Version 3
     trickv3() // Body of lambda expression is executed
     trickFunctionV3()
+    treat()
+    println()
 
+    val treatFunctionV4 = trickOrTreat(false)
+    val trickFunctionV4 = trickOrTreat(true)
+    treatFunctionV4()
+    trickFunctionV4()
+    println()
+
+    val coins: (Int) -> String = { quantity ->
+        "$quantity quarters"
+    }
+
+    val cupcake: (Int) -> String = {
+        "Have a cupcake"
+    }
+
+    val treatFunctionV5 = trickOrTreatV2(false, coins)
+    val trickFunctionV5 = trickOrTreatV2(true, cupcake)
+    treatFunctionV5()
+    trickFunctionV5()
+    println()
+
+    val treatFunctionV5_2 = trickOrTreatV2(false, cupcake)
+    treatFunctionV5_2()
+    println()
+
+    val treatFunctionV6 = trickOrTreatV3(false, coins)
+    val trickFunctionV6 = trickOrTreatV3(true, null)
+    treatFunctionV6()
+    trickFunctionV6()
 }
 
 // Version 1
@@ -32,4 +62,39 @@ val trickv3 = {
     println("No treats!")
 }
 
+// Specifying variable's data type as Unit
+val treat: () -> Unit = {
+    println("Have a treat!")
+}
+
+// Functions can return other functions
+fun trickOrTreat(isTrick: Boolean): () -> Unit {
+    if (isTrick) {
+        return trickv3
+    } else {
+        return treat
+    }
+}
+
+// Pass a function to another function as an argument
+fun trickOrTreatV2(isTrick: Boolean, extraTreat: (Int) -> String): () -> Unit {
+    if (isTrick) {
+        return trickv3
+    } else {
+        println(extraTreat(5))
+        return treat
+    }
+}
+
+// Nullable function types
+fun trickOrTreatV3(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+    if (isTrick) {
+        return trickv3
+    } else {
+        if (extraTreat != null) {
+            println(extraTreat(5))
+        }
+        return treat
+    }
+}
 
