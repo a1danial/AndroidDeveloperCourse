dd7c3c057404c9f69f8394c323dbd1bc0b474b06
Danny <danny@mymy.cards>
2022-05-16 20:56:43 +0800

    Completed: Unit 2: Building app UI: Kotlin fundamentals: 5. Use function types and lambda expressions in Kotlin Complete: 2 Storing function as a variable

--- app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path1_5.kt
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path1_5.kt
@@ -1,10 +1,35 @@
 package com.example.androiddevelopercourse
 
 fun main() {
-    val trickFunction = ::trick
+    // Function invocation 'trick()' expected ; as it recognises trick as the name of trick() function
+//    val trickFunctionV1 = trickv1 // Version 1
+
+    // Function reference operator :: needed to refer function as value
+    val trickFunctionV2 = ::trickv2 // Version 2
+
+    // Function reference operator :: removed because trick now refers to a variable rather
+    // than a function name
+    val trickFunctionV3 = trickv3 // Version 3
+    trickv3() // Body of lambda expression is executed
+    trickFunctionV3()
+
 }
 
-fun trick() {
+// Version 1
+fun trickv1() {
     println("No treats!")
 }
+
+// Version 2
+fun trickv2() {
+    println("No treats!")
+}
+
+// Version 3
+// With lambda expressions, you can create variables that store functions, call these variables
+// like functions, and store them in other variables that you can call like functions
+val trickv3 = {
+    println("No treats!")
+}
+
 
