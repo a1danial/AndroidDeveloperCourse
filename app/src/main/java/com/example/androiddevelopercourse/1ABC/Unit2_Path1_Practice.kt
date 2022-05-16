bad0417b82a9ca1e805c20e9e29a507364050f4d
Danny <danny@mymy.cards>
2022-05-16 22:21:42 +0800

    Completed: Unit 2: Building app UI: Kotlin fundamentals: 5. Use function types and lambda expressions in Kotlin Practice 2: Complete

--- app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path1_Practice.kt
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path1_Practice.kt
@@ -1,4 +1,20 @@
 package com.example.androiddevelopercourse
 
+fun main() {
+    // Practice 2
+    val morningNotification = 51
+    val eveningNotification = 135
 
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+// Practice 2
+fun printNotificationSummary(numberOfMessages: Int) {
+    if (numberOfMessages < 100 ) {
+        println("You have $numberOfMessages notifications.")
+    } else {
+        println("Your phone is blowing up! You have 99+ notifications.")
+    }
+}
 
