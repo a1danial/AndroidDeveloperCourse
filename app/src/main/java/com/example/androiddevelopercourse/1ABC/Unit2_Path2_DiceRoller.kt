2e56d0db50189e3ebbbae00dec1e803d7dd589f1
Ahmad Danial Basaruddin <zcapadb@ucl.ac.uk>
2022-05-17 19:05:55 +0800

    Completed: Unit 2: Building app UI: Path 2: Add a button Completed 6: Add an image

--- app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path2_DiceRoller.kt
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path2_DiceRoller.kt
@@ -4,9 +4,8 @@ package com.example.androiddevelopercourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -14,8 +13,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevelopercourse.ui.theme.AndroidDeveloperCourseTheme
 
 class Unit2_Path2_DiceRoller : ComponentActivity() {
@@ -37,6 +39,11 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier
     Column(modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(painter = painterResource(R.drawable.dice_1),
+            contentDescription = "1")
+        
+        Spacer(modifier = Modifier.height(16.dp))
+        
         Button(onClick = { /*TODO*/ }) {
             Text(stringResource(R.string.roll))
         }
