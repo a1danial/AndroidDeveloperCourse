6a35fb38d40e4e1502b6b77648fc7d7c71eb6412
Ahmad Danial Basaruddin <zcapadb@ucl.ac.uk>
2022-05-17 18:53:03 +0800

    Completed: Unit 2: Building app UI: Path 2: Add a button Completed 4: Create a vertical layout and 5: Add a button

--- app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path2_DiceRoller.kt
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path2_DiceRoller.kt
@@ -4,14 +4,17 @@ package com.example.androiddevelopercourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androiddevelopercourse.ui.theme.AndroidDeveloperCourseTheme
 
@@ -31,9 +34,17 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier
     .fillMaxSize()
     .wrapContentSize(Alignment.Center)
 ) {
+    Column(modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.roll))
+        }
+    }
 
 }
 
+@Preview
 @Composable
 fun DiceRollerApp() {
     DiceWithButtonAndImage()
