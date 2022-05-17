abeca1e2864e6fa3f3ee1bd9df686bbb104cc802
Ahmad Danial Basaruddin <zcapadb@ucl.ac.uk>
2022-05-17 18:44:36 +0800

    Completed: Unit 2: Building app UI: Path 2: Add a button Completed 3: Create the layout infrastructure and prev.

--- app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path2_DiceRoller.kt
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path2_DiceRoller.kt
@@ -1,4 +1,41 @@
 package com.example.androiddevelopercourse
 
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.androiddevelopercourse.ui.theme.AndroidDeveloperCourseTheme
+
+class Unit2_Path2_DiceRoller : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidDeveloperCourseTheme { // Follows name of project
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)
+) {
+
+}
+
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
+}
 
