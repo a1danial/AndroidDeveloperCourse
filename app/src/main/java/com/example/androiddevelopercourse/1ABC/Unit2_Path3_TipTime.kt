6257ab22286a8f2280218c3db42104f499c275a4
Danny <danialbasaruddin@gmail.com>
2022-05-18 16:39:35 +0800

    Completed: Unit 2: Building app UI Path 3: UI and State Class 3: Intro to state in Compose _________________________ Display the tip amount (page 10): Completed

--- app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path3_TipTime.kt
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path3_TipTime.kt
@@ -14,12 +14,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.sp
 
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androiddevelopercourse.ui.theme.AndroidDeveloperCourseTheme
+import java.text.NumberFormat
 
 class Unit2_Path3_TipTime : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,12 +51,24 @@ fun TipTimeScreen() {
         )
         Spacer(Modifier.height(16.dp))
         EditNumberField()
+        Spacer(Modifier.height(24.dp))
+        Text(
+            text = stringResource(id = R.string.tip_amount, ""),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
 @Composable
 fun EditNumberField() {
     var amountInput by remember { mutableStateOf("") }
+
+    // parses a string as a Double number and returns the result or null if the string isn't a valid representation of a number
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+
+    val tip = calculateTip(amount)
 
     TextField(
         value = amountInput,
@@ -64,6 +78,14 @@ fun EditNumberField() {
         singleLine = true, // single horizontally scrolling text field
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number) // set keyboard type to number keyboard
     )
+}
+
+private fun calculateTip(
+    amount: Double,
+    tipPercent: Double = 15.0
+): String {
+    val tip = amount * (tipPercent / 100)
+    return NumberFormat.getCurrencyInstance().format(tip)
 }
 
 @Preview (showSystemUi = true)
