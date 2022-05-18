907599ddd410377b87af4b1562253c78a9cb9f76
Danny <danialbasaruddin@gmail.com>
2022-05-18 14:55:59 +0800

    Completed: Unit 2: Building app UI Path 3: UI and State Class 3: Intro to state in Compose _________________________ Page 4 (Add a screen title) Complete

--- app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path3_TipTime.kt
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path3_TipTime.kt
@@ -1,4 +1,69 @@
 package com.example.androiddevelopercourse
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevelopercourse.ui.theme.AndroidDeveloperCourseTheme
+
+class Unit2_Path3_TipTime : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidDeveloperCourseTheme { // Follows name of project
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TipTimeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TipTimeScreen() {
+    Column(
+        modifier = Modifier.padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(text = stringResource(id = R.string.calculate_tip),
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Spacer(Modifier.height(16.dp))
+        EditNumberField()
+    }
+}
+
+@Composable
+fun EditNumberField() {
+    TextField(
+        value = "",
+        onValueChange = {},
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Preview (showSystemUi = true)
+@Composable
+fun TipTimeDefaultPreview() {
+    AndroidDeveloperCourseTheme {
+        TipTimeScreen()
+    }
+}
 
