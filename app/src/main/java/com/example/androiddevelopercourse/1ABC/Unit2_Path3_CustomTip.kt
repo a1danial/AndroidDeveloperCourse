e38cc66d1736f475495acc477dc87a481c86a345
Danny <danialbasaruddin@gmail.com>
2022-05-23 18:45:05 +0800

    Unit 2: Building app UI Path 3: UI and State Class 4: Calculate a custom tip _________________________ Add a switch (page 6): Completed

--- app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path3_CustomTip.kt
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path3_CustomTip.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -45,15 +46,17 @@ class Unit2_Path3_CustomTip : ComponentActivity() {
 fun CustomTipScreen() {
     var amountInput by remember { mutableStateOf("") }
     var tipInput by remember { mutableStateOf("") }
+    var roundUp by remember { mutableStateOf(false) }
 
     // parses a string as a Double number and returns the result or null if the string isn't a valid representation of a number
     val amount = amountInput.toDoubleOrNull() ?: 0.0
     val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
 
-    val tip = calculateTip(amount, tipPercent)
+    val tip = calculateTip(amount, tipPercent, roundUp)
 
     // Variable to move focus to, and clear the focus from
     val focusManager = LocalFocusManager.current
+
 
     Column(
         modifier = Modifier.padding(32.dp),
@@ -88,12 +91,38 @@ fun CustomTipScreen() {
             value = tipInput,
             onValueChange = { tipInput = it }
         )
+        RoundTheTipRow(roundUp = roundUp, onRoundUpChanged = { roundUp = it })
         Spacer(Modifier.height(24.dp))
         Text(
             text = stringResource(id = R.string.tip_amount, tip),
             modifier = Modifier.align(Alignment.CenterHorizontally),
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun RoundTheTipRow(
+    roundUp: Boolean,
+    onRoundUpChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .size(48.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = stringResource(id = R.string.round_up_tip))
+        Switch(modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.End),
+            checked = roundUp,
+            onCheckedChange = onRoundUpChanged,
+            colors = SwitchDefaults.colors(
+                uncheckedThumbColor = Color.DarkGray
+            )
         )
     }
 }
@@ -125,9 +154,13 @@ fun EditNumberFieldCustomTip(
 
 private fun calculateTip(
     amount: Double,
-    tipPercent: Double = 15.0
+    tipPercent: Double = 15.0,
+    roundUp: Boolean
 ): String {
-    val tip = amount * (tipPercent / 100)
+    var tip = amount * (tipPercent / 100)
+    if (roundUp) {
+        tip = kotlin.math.ceil(tip)
+    }
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
