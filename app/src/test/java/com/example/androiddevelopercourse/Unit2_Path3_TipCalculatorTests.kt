1f53cafeb231741ea8b0dc7fd9527aedc525bc84
Danny <danialbasaruddin@gmail.com>
2022-05-24 12:34:48 +0800

    Unit 2: Building app UI Path 3: UI and State Class 5: Write automated tests _________________________ Write a local test (page 4): Complete

---
+++ app/src/test/java/com/example/androiddevelopercourse/Unit2_Path3_TipCalculatorTests.kt
@@ -0,0 +1,21 @@
+package com.example.androiddevelopercourse
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class Unit2_Path3_TipCalculatorTests {
+
+    @Test
+    fun calculate_20_percent_tip_no_roundup() {
+
+    }
+}
+
+@Test
+fun calculate_20_percent_tip_no_roundup() {
+    val amount = 10.00
+    val tipPercent = 20.00
+    val expectedTip = "$2.00"
+    val actualTip = calculateCustomTip(amount, tipPercent, false)
+    assertEquals(expectedTip, actualTip)
+}
