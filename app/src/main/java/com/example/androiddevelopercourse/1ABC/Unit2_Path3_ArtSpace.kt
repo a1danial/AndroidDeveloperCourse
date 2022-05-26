cd0c8ca89a61c36e1bb61f1bbe08ddb40bf93548
Ahmad Danial Basaruddin <zcapadb@ucl.ac.uk>
2022-05-26 21:41:58 +0800

    Unit 2: Building app UI Path 3: UI and State Class 5: Create an Art Space app _________________________ Layout complete

--- app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path3_ArtSpace.kt
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path3_ArtSpace.kt
@@ -13,9 +13,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androiddevelopercourse.ui.theme.AndroidDeveloperCourseTheme
 
 @Composable
@@ -37,20 +43,38 @@ fun ArtSpaceScreen() {
                 .width(200.dp)
                 .border(5.dp, Color.Gray)
         )
-        Spacer(modifier = Modifier.size(30.dp))
+        Spacer(modifier = Modifier.size(15.dp))
         Column(
             modifier = Modifier
                 .border(2.dp, Color.Gray)
-                .width(300.dp)
-                .padding(start = 30.dp, end = 30.dp, top = 10.dp, bottom = 10.dp),
+                .width(275.dp)
+                .padding(start = 20.dp, end = 20.dp, top = 10.dp, bottom = 10.dp),
             horizontalAlignment = Alignment.Start,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.spacedBy(5.dp, Alignment.CenterVertically)
         ) {
-            Text(text = "Apollo and Daphne")
-            Row() {
-                Text(text = "Gian Lorenzo Bernini")
-                Text(text = "(1625)")
-            }
+            Text(text = "Apollo and Daphne",
+                textAlign = TextAlign.Start,
+                fontSize = 30.sp
+            )
+            Text(
+                buildAnnotatedString {
+                    withStyle(style = SpanStyle(
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    ) {
+                        append("Gian Lorenzo Bernini")
+                    }
+                    withStyle(style = SpanStyle(
+                        fontSize = 15.sp,
+                        color = Color.LightGray,
+                        fontStyle = FontStyle.Italic
+                    )
+                    ) {
+                        append("  (1625)")
+                    }
+                },
+            )
         }
         Spacer(modifier = Modifier.size(30.dp))
         Row(verticalAlignment = Alignment.CenterVertically,
