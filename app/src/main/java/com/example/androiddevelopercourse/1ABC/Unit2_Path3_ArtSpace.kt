bf3f96d3248cc91d62f371bf2a6a3df7125b3851
Ahmad Danial Basaruddin <zcapadb@ucl.ac.uk>
2022-05-26 20:43:59 +0800

    Unit 2: Building app UI Path 3: UI and State Class 5: Create an Art Space app _________________________ Background: White background with grey borders

--- app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path3_ArtSpace.kt
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit2_Path3_ArtSpace.kt
@@ -1,4 +1,36 @@
 package com.example.androiddevelopercourse
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevelopercourse.ui.theme.AndroidDeveloperCourseTheme
 
+@Composable
+fun ArtSpaceScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White).border(5.dp, Color.Gray),
+    ) {
+
+    }
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun ArtSpaceDefaultPreview() {
+    AndroidDeveloperCourseTheme {
+        ArtSpaceScreen()
+    }
+}
 
