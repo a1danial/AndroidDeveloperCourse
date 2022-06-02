b4cd6d45982d41a2079a1a976f64482b0a1838ea
Ahmad Danial Basaruddin <zcapadb@ucl.ac.uk>
2022-06-02 13:29:59 +0800

    Course: Android Basics in Kotlin Unit 3: Navigation Path 1: Navigate between screens Module 1: Collections in Kotlin _________________________ Learn about collections (step 2): Complete _________________________ Next step: - N/A

--- app/src/main/java/com/example/androiddevelopercourse/2ABK/U3_P1.kt
+++ app/src/main/java/com/example/androiddevelopercourse/2ABK/U3_P1.kt
@@ -1,3 +1,25 @@
 package com.example.androiddevelopercourse
 
 
+fun main() {
+    /* Create a list and sets */
+//    val numbers = listOf(0, 3, 8, 4, 0, 5, 5, 8, 9, 2)
+//    println("list:   ${numbers}")
+//    println("sorted: ${numbers.sorted()}")
+//    val setOfNumbers = numbers.toSet()
+//    println("set:    ${setOfNumbers}")
+//    val set1 = setOf(1,2,3)
+//    val set2 = mutableSetOf(3,2,1)
+//    println("$set1 == $set2: ${set1 == set2}")
+//    println("contains 7: ${setOfNumbers.contains(7)}")
+
+    /* Learn about maps */
+    val peopleAges = mutableMapOf<String, Int>(
+        "Fred" to 30,
+        "Ann" to 23
+    )
+    peopleAges.put("Barbara", 42)
+    peopleAges["Joe"] = 51
+    peopleAges["Fred"] = 31 // No new key is added but value is updated
+    println(peopleAges)
+}
