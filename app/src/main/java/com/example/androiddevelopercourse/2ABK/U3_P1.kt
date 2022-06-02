2284702f2ffd60e81137d6d52459b6eb8d1ee9df
Ahmad Danial Basaruddin <zcapadb@ucl.ac.uk>
2022-06-02 13:34:17 +0800

    Course: Android Basics in Kotlin Unit 3: Navigation Path 1: Navigate between screens Module 2: Collections in Kotlin (correction) _________________________ Reorganise _________________________ Next step: - N/A

--- app/src/main/java/com/example/androiddevelopercourse/2ABK/U3_P1.kt
+++ app/src/main/java/com/example/androiddevelopercourse/2ABK/U3_P1.kt
@@ -2,16 +2,20 @@ package com.example.androiddevelopercourse
 
 
 fun main() {
+    ABK_U3_P1_M2() // Module 2: Navigate between screens
+}
+
+fun ABK_U3_P1_M2() {
     /* Create a list and sets */
-//    val numbers = listOf(0, 3, 8, 4, 0, 5, 5, 8, 9, 2)
-//    println("list:   ${numbers}")
-//    println("sorted: ${numbers.sorted()}")
-//    val setOfNumbers = numbers.toSet()
-//    println("set:    ${setOfNumbers}")
-//    val set1 = setOf(1,2,3)
-//    val set2 = mutableSetOf(3,2,1)
-//    println("$set1 == $set2: ${set1 == set2}")
-//    println("contains 7: ${setOfNumbers.contains(7)}")
+    val numbers = listOf(0, 3, 8, 4, 0, 5, 5, 8, 9, 2)
+    println("list:   ${numbers}")
+    println("sorted: ${numbers.sorted()}")
+    val setOfNumbers = numbers.toSet()
+    println("set:    ${setOfNumbers}")
+    val set1 = setOf(1,2,3)
+    val set2 = mutableSetOf(3,2,1)
+    println("$set1 == $set2: ${set1 == set2}")
+    println("contains 7: ${setOfNumbers.contains(7)}")
 
     /* Learn about maps */
     val peopleAges = mutableMapOf<String, Int>(
