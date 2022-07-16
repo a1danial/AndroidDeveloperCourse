d5fedee9b390c0d1244ec62a4c83a3456216ab82
Ahmad Danial Basaruddin <zcapadb@ucl.ac.uk>
2022-07-16 22:53:28 +0800

    Course: Android Basics in Kotlin Unit 5: Data persistence Path 1: Introduction to SQL, Room, and Flow Module 1: SQL Basics _________________________ Step 1 - 5: Complete _________________________ Next step: - N/A

--- app/src/main/java/com/example/androiddevelopercourse/MainActivity.kt
+++ app/src/main/java/com/example/androiddevelopercourse/MainActivity.kt
@@ -12,28 +12,36 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androiddevelopercourse.ui.theme.AndroidDeveloperCourseTheme
 import com.example.diceroller.DiceRollerAppDebug
+import com.example.sqlbasics.AppDatabase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidDeveloperCourseTheme {
-                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-//                ) {
-//                    Greeting("Android")
-//                }
+//            AndroidDeveloperCourseTheme {
+//                // A surface container using the 'background' color from the theme
+////                Surface(
+////                    modifier = Modifier.fillMaxSize(),
+////                    color = MaterialTheme.colors.background
+////                ) {
+////                    Greeting("Android")
+////                }
+//
+////                DiceRollerApp() // Unit 2; Pathway 2; Page 7
+////                DiceRollerAppDebug() // Unit 2; Pathway 2; Class 3; Page 3
+//
+////                LemonadeApp() // Unit 2; Pathway 2; Class 4
+//
+////                TipTimeScreen() // Unit 2; Pathway 3; Class 3
+////                CustomTipScreen() // Unit 2; Pathway 3; Class 4
+////                ArtSpaceScreen() // Unit 2; Pathway 3; Class 5
+//            }
 
-//                DiceRollerApp() // Unit 2; Pathway 2; Page 7
-//                DiceRollerAppDebug() // Unit 2; Pathway 2; Class 3; Page 3
+            GlobalScope.launch {
+                AppDatabase.getDatabase(applicationContext).californiaParkDao().getAll()
 
-//                LemonadeApp() // Unit 2; Pathway 2; Class 4
-
-//                TipTimeScreen() // Unit 2; Pathway 3; Class 3
-//                CustomTipScreen() // Unit 2; Pathway 3; Class 4
-                ArtSpaceScreen() // Unit 2; Pathway 3; Class 5
             }
         }
     }
