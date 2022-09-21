ddd44f5b7f28674b0f11a487b87e5880d7c8d863
Ahmad Danial Basaruddin <zcapadb@ucl.ac.uk>
2022-09-21 16:21:27 +0800

    Course: Android Basics with Compose Unit 3: Display lists and use Material Design Path 1: More Kotlin fundamentals Module 1 (Intro) - Module 5 (Use a singleton object)

---
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit3_Path1.kt
@@ -0,0 +1,32 @@
+package com.example.androiddevelopercourse
+
+class Unit3_Path1 {
+
+}
+
+data class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: Difficulty
+    )
+
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+
+class Quiz {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
+fun main(args: Array<String>) {
+//    println(question1.toString())
+    println("${Quiz.answered} of ${Quiz.total} answered")
+}
+
