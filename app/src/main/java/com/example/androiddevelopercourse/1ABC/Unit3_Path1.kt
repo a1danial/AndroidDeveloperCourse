52122f7fff982837df2dae6fe1e6abe87559fb3a
Ahmad Danial Basaruddin <zcapadb@ucl.ac.uk>
2022-09-21 21:48:30 +0800

    Course: Android Basics with Compose Unit 3: Display lists and use Material Design Path 2: Use collections in Kotlin Module 1 (Intro) - Module 2 (Arrays in Kotlin)

--- app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit3_Path1.kt
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit3_Path1.kt
@@ -74,20 +74,20 @@ fun main(args: Array<String>) {
     val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
     val solarSystem = rockPlanets + gasPlanets
 
-    println(solarSystem[0])
-    println(solarSystem[1])
-    println(solarSystem[2])
-    println(solarSystem[3])
-    println(solarSystem[4])
-    println(solarSystem[5])
-    println(solarSystem[6])
-    println(solarSystem[7])
+//    println(solarSystem[0])
+//    println(solarSystem[1])
+//    println(solarSystem[2])
+//    println(solarSystem[3])
+//    println(solarSystem[4])
+//    println(solarSystem[5])
+//    println(solarSystem[6])
+//    println(solarSystem[7])
 
     solarSystem[3] = "Little Earth"
-    println(solarSystem[3])
+//    println(solarSystem[3])
 
 //    solarSystem[8] = "Pluto" // ArrayIndexOutOfBoundsException: Index 8 out of bounds for length 8
     val newSolarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
-    println(newSolarSystem[8])
+//    println(newSolarSystem[8])
 }
 
