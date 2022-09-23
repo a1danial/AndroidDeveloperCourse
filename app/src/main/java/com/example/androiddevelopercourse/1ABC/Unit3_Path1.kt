726e9874b080063d8dfc3074404c7e3e8328f6b4
Ahmad Danial Basaruddin <zcapadb@ucl.ac.uk>
2022-09-23 12:06:34 +0800

    Course: Android Basics with Compose Unit 3: Display lists and use Material Design Path 1: More Kotlin Fundamentals Module 4: Higher-order functions with collections _________________________ Step 2 Complete

--- app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit3_Path1.kt
+++ app/src/main/java/com/example/androiddevelopercourse/1ABC/Unit3_Path1.kt
@@ -56,6 +56,58 @@ fun Quiz.StudentProgress.printProgressBar() {
     println(Quiz.progressText)
 }
 
+class Cookie(
+    val name: String,
+    val softBaked: Boolean,
+    val hasFilling: Boolean,
+    val price: Double
+)
+
+val cookies = listOf(
+    Cookie(
+        name = "Chocolate chip",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.69
+    ),
+    Cookie(
+        name = "Banana Walnut",
+        softBaked = true,
+        hasFilling = false,
+        price = 1.49
+    ),
+    Cookie(
+        name = "Vanilla Creme",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.59
+    ),
+    Cookie(
+        name = "Chocolate Peanut Butter",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.49
+    ),
+    Cookie(
+        name = "Snickerdoodle",
+        softBaked = true,
+        hasFilling = false,
+        price = 1.39
+    ),
+    Cookie(
+        name = "Blueberry Tart",
+        softBaked = true,
+        hasFilling = true,
+        price = 1.79
+    ),
+    Cookie(
+        name = "Sugar and Sprinkles",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.39
+    )
+)
+
 fun main(args: Array<String>) {
 //    println(question1.toString())
 //    println(Quiz.progressText)
@@ -126,26 +178,32 @@ fun main(args: Array<String>) {
 //    println(solarSystem.contains("Pluto"))
 
     // Step 5: Map collection
-    val solarSystem = mutableMapOf(
-        "Mercury" to 0,
-        "Venus" to 0,
-        "Earth" to 1,
-        "Mars" to 2,
-        "Jupiter" to 79,
-        "Saturn" to 82,
-        "Uranus" to 27,
-        "Neptune" to 14
-    )
-    println(solarSystem.size)
-    solarSystem["Pluto"] = 5
-    println(solarSystem.size)
-    println(solarSystem["Pluto"])
-    println(solarSystem["Theia"])
+//    val solarSystem = mutableMapOf(
+//        "Mercury" to 0,
+//        "Venus" to 0,
+//        "Earth" to 1,
+//        "Mars" to 2,
+//        "Jupiter" to 79,
+//        "Saturn" to 82,
+//        "Uranus" to 27,
+//        "Neptune" to 14
+//    )
+//    println(solarSystem.size)
+//    solarSystem["Pluto"] = 5
+//    println(solarSystem.size)
+//    println(solarSystem["Pluto"])
+//    println(solarSystem["Theia"])
+//
+//    solarSystem.remove("Pluto")
+//    println(solarSystem.size)
+//
+//    solarSystem["Jupiter"] = 78
+//    println(solarSystem["Jupiter"])
 
-    solarSystem.remove("Pluto")
-    println(solarSystem.size)
-
-    solarSystem["Jupiter"] = 78
-    println(solarSystem["Jupiter"])
+    // MODULE 4
+    // Step 2
+    cookies.forEach {
+        println("Menu item: ${it.name}")
+    }
 }
 
