d62e9e25ba9a1362410c7b637b0dbc5202fa41d6
Ahmad Danial Basaruddin <zcapadb@ucl.ac.uk>
2022-09-29 22:10:49 +0800

    Course: Android Basics with Compose Unit 3: Display lists and use Material Design Path 3: Build beautiful apps Module 3: Simple Animation with Jetpack Compose _________________________ Step 1: Before you begin -> Step 10: Conclusion

--- app/src/main/java/com/example/androiddevelopercourse/A1ABC/Unit3_Path3/Woof.kt
+++ app/src/main/java/com/example/androiddevelopercourse/A1ABC/Unit3_Path3/Woof.kt
@@ -2,20 +2,25 @@ package com.example.androiddevelopercourse.A1ABC.Unit3_Path3
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -23,6 +28,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androiddevelopercourse.ui.theme.WoofTheme
 import com.example.androiddevelopercourse.R
+import com.example.androiddevelopercourse.ui.theme.Green100
+import com.example.androiddevelopercourse.ui.theme.Green50
+import com.example.androiddevelopercourse.ui.theme.Grey50
 
 @Composable
 fun WoofTopAppBar(modifier: Modifier = Modifier) {
@@ -59,7 +67,9 @@ fun WoofApp() {
         }
     ) { padding ->
         LazyColumn(
-            modifier = Modifier.background(MaterialTheme.colors.background).padding(padding)
+            modifier = Modifier
+                .background(MaterialTheme.colors.background)
+                .padding(padding)
         ) {
             items(dogs) {
                 DogItem(dog = it)
@@ -76,19 +86,57 @@ fun WoofApp() {
  */
 @Composable
 fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+    val color by animateColorAsState(
+        targetValue = if (expanded) Green100 else MaterialTheme.colors.surface
+    )
     Card(
         modifier = modifier.padding(8.dp),
         elevation = 4.dp
     ) {
-        Row(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-//                .background(MaterialTheme.colors.surface) // Remove as color in theme explicity set for surface (card)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
+                .background(color = color)
         ) {
-            DogIcon(dog.imageResourceId)
-            DogInformation(dog.name, dog.age)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+//                .background(MaterialTheme.colors.surface) // Remove as color in theme explicity set for surface (card)
+            ) {
+                DogIcon(dog.imageResourceId)
+                DogInformation(dog.name, dog.age)
+                Spacer(Modifier.weight(1f))
+                DogItemButton(
+                    expanded = expanded,
+                    onClick = { expanded = !expanded }
+                )
+            }
+            if (expanded) {
+                DogHobby(dog.hobbies)
+            }
         }
+    }
+}
+
+@Composable
+private fun DogItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            tint = MaterialTheme.colors.secondary,
+            contentDescription = stringResource(R.string.expand_button_content_description)
+        )
     }
 }
 
@@ -134,6 +182,30 @@ fun DogInformation(@StringRes dogName: Int, dogAge: Int, modifier: Modifier = Mo
         Text(
             text = stringResource(R.string.years_old, dogAge),
 //            color = MaterialTheme.colors.onSurface // Remove as color in theme explicity set for surface (card)
+            style = MaterialTheme.typography.body1
+        )
+    }
+}
+
+@Composable
+fun DogHobby(
+    @StringRes dogHobby: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(
+            start = 16.dp,
+            top = 8.dp,
+            bottom = 16.dp,
+            end = 16.dp
+        )
+    ) {
+        Text(
+            text = stringResource(R.string.about),
+            style = MaterialTheme.typography.h3
+        )
+        Text(
+            text = stringResource(dogHobby),
             style = MaterialTheme.typography.body1
         )
     }
